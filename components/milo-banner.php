91beabd13a3002342c54c1e97e87f9f1b297b4be
tallulahkay <tallulah.kay@gmail.com>
2026-09-05 21:37:44 -0700

    update banner

--- components/milo-banner.php
+++ components/milo-banner.php
@@ -11,6 +11,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 ?>
 
-<p class="milo-banner">
-	<b>Milo Point Richmond is closing for renovations!</b> Labor Day, Monday, September 7th, we close at 5pm, hoping all dogs and cats are adopted or in foster. We re-open early October! It will be exciting, shiny and new!
-</p>
+<div class="milo-banner">
+	<p class="milo-banner__announcement">
+		<b>Milo Point Richmond is closing for renovations!</b> Labor Day, Monday, September 7th, we close at 5pm, hoping all dogs and cats are adopted or in foster. We re-open early October! It will be exciting, shiny and new!
+	</p>
+	<p class="milo-banner__adoption-message">
+		We are still caring for adoptable animals in foster or at our Sanctuary. Apply to adopt today!
+	</p>
+</div>
